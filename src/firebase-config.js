c3255e8bedba23167e6834428359817c06ef7114
Your Name <your-email@example.com>
2024-12-09 01:58:15 +0600

    GOOGLE SIGN ADD commit

--- src/firebase-config.js
+++ src/firebase-config.js
@@ -1,16 +1,19 @@
-// src/firebase-config.js
-import { initializeApp } from 'firebase/app';
-import { getAuth } from 'firebase/auth';
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
 
+// Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: 'YOUR_API_KEY',
-  authDomain: 'YOUR_AUTH_DOMAIN',
-  projectId: 'YOUR_PROJECT_ID',
-  storageBucket: 'YOUR_STORAGE_BUCKET',
-  messagingSenderId: 'YOUR_MESSAGING_SENDER_ID',
-  appId: 'YOUR_APP_ID',
+  apiKey: "AIzaSyAB_lFSB3VsSGS2qXAUWFi29t6_MCxW9gk",
+  authDomain: "r-estate-a9.firebaseapp.com",
+  projectId: "r-estate-a9",
+  storageBucket: "r-estate-a9.appspot.com",
+  messagingSenderId: "721717896380",
+  appId: "1:721717896380:web:f888dea10c8ca7909cdcb9",
+  measurementId: "G-XLE517WKLB",
 };
 
+// Initialize Firebase
 const app = initializeApp(firebaseConfig);
 const auth = getAuth(app);
 
